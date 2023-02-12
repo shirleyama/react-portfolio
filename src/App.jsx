5a7ec82bad72dd27243ef53929dafa7494d3cb7e
Ama Ama <quists@hotmail.com>
2023-02-12 01:34:30 +0000

    created pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
-import About from "./components/about/About";
-import Skills from "./components/skills/Skills";
-import Projects from "./components/projects/Projects";
-import Contact from "./components/contact/Contact";
+import About from "./pages/about/About";
+import Skills from "./pages/skills/Skills";
+import Projects from "./pages/projects/Projects";
+import Contact from "./pages/contact/Contact";
 import Footer from "./components/footer/Footer";
 
 const App = () => {
