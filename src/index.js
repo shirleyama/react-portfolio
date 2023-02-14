ca0030bccb74c78951f2d12973a794c402999b7a
Ama Ama <quists@hotmail.com>
2023-02-14 22:56:32 +0000

    added home url

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import App from "./App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
